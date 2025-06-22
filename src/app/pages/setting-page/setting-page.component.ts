14df4071cc4d9c90dc908cabeb18ac0481e645a8
beksu <Beksuktan7@gmail.com>
2025-06-22 21:11:34 +0600

    Добавил сохранение Аватарки и Стек технелогий в редактирование Анкеты пользователя

--- src/app/pages/setting-page/setting-page.component.ts
+++ src/app/pages/setting-page/setting-page.component.ts
@@ -1,24 +1,73 @@
-import {Component, inject} from '@angular/core';
+import {Component, effect, inject, ViewChild} from '@angular/core';
 import {ProfileHeaderComponent} from '../../common-ui/profile-header/profile-header.component';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {ProfileService} from '../../data/services/profile.service';
+import {firstValueFrom} from 'rxjs';
+import {AvatarUploadComponent} from './avatar-upload/avatar-upload.component';
 
 @Component({
   selector: 'app-setting-page',
   imports: [
     ProfileHeaderComponent,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AvatarUploadComponent
   ],
   templateUrl: './setting-page.component.html',
   styleUrl: './setting-page.component.scss'
 })
 export class SettingPageComponent {
   fb = inject(FormBuilder)
+  profileService = inject(ProfileService)
+
+  @ViewChild(AvatarUploadComponent) avatarUploader!: AvatarUploadComponent
 
   form = this.fb.group({
     firstName: ['', Validators.required],
     lastName: ['', Validators.required],
-    userName: ['', Validators.required],
+    username: [{value: '',disabled: true}, Validators.required],
     description: [''],
     stack: ['']
   })
+
+  constructor() {
+    effect(() => {
+      //@ts-ignore
+      this.form.patchValue({
+        ...this.profileService.me(),
+        //@ts-ignore
+        stack: this.mergeStack(this.profileService.me()?.stack)
+      })
+    });
+  }
+
+  onSave(): void {
+    this.form.markAllAsTouched()
+    this.form.updateValueAndValidity()
+
+    if (this.form.invalid) return;
+
+    if (this.avatarUploader.avatar) {
+      firstValueFrom(this.profileService.uploadAvatar(this.avatarUploader.avatar))
+    }
+
+    //@ts-ignore
+    firstValueFrom(this.profileService.patchProfile({
+      ...this.form.value,
+      stack: this.splitStack(this.form.value.stack)
+    }))
+  }
+
+  splitStack(stack: string | null | string[] | undefined) : string[] {
+    if (!stack) return [];
+    if (Array.isArray(stack)) return stack;
+
+    return stack.split(',')
+  }
+
+  mergeStack(stack: string | null | string[] | undefined) {
+    if (!stack) return '';
+    if (Array.isArray(stack)) return stack.join(',');
+
+    return stack
+  }
 }
